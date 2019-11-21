7e655670225c82bd507f8204f86766ec182b77e4
017Alex <wutong_017@163.com>
2019-11-21 22:05:17 +0800

    C++

---
+++ C++/20191121/20191121/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Hello World!" << endl;
+	int a = 10;
+	char c = 'ABC';
+	float b = 3.1415926;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+}
